e95f323ce87a60b8170b99dc7de22daeb7a9eacf
Muhammad Ridwan Na'im <noplanalderson@gmail.com>
2020-11-04 03:18:29 +0700

    Modify Welcome Page and Create Form Upload

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,6 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
+	protected $image = array(
+		'file_name' => '',
+		'image_type' => '',
+		'image_size_str' => array('width' => '', 'height' => ''),
+		'file_size' => '',
+		'full_path' => ''
+	);
+
+	protected $error = '';
+
 	/**
 	 * Index Page for this controller.
 	 *
@@ -20,6 +30,52 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		if(isset($_POST['upload']))
+		{
+			$filename = pathinfo($_FILES['image']['name'], PATHINFO_FILENAME);
+			$filename = preg_replace("/[^a-zA-Z0-9 \-_.]+/i", '', $filename);
+			$filename = str_replace(' ', '-', $filename);
+
+			$config = array(
+				'form_name' => 'image',
+				'upload_path' => FCPATH . 'uploads',
+				'allowed_types' => 'png|jpg|jpeg|webp',
+				'max_size' => '5128',
+				'detect_mime' => TRUE,
+				'file_ext_tolower' => TRUE,
+				'overwrite' => TRUE,
+				'add_salt' => TRUE,
+				'file_name' => $filename,
+				'extension' => 'webp',
+				'quality' => '100%',
+				'maintain_ratio' => TRUE,
+				'width' => 800,
+				'height' => 600
+			);
+
+			$this->load->library('secure_upload');
+			$this->secure_upload->initialize($config);
+
+			if($this->secure_upload->doUpload())
+			{
+				$this->image = $this->secure_upload->data();
+			}
+			else
+			{
+				$this->error = $this->secure_upload->show_errors();
+			}
+
+			$this->secure_upload->removeOriginalImage();
+
+			$data['image'] = $this->image;
+			$data['error'] = $this->error;
+		}
+		else
+		{
+			$data['image'] = $this->image;
+			$data['error'] = $this->error;
+		}
+
+		$this->load->view('welcome_message', $data);
 	}
 }
