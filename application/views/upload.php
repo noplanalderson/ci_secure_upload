d454337b1d2dfc33da20ac95e11006b6fcc562fc
Muhammad Ridwan Na'im <noplanalderson@gmail.com>
2020-11-06 09:11:58 +0700

    Update Credit(s)

--- application/views/upload.php
+++ application/views/upload.php
@@ -118,7 +118,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			?>
 		</code>
 
-		<p class="credits">Credits : Muhammad Ridwan Na'im, MTCNA & Anrie 'Riesurya' Suryaningrat, S.Si, M.TI, Apt.</p>
+		<p class="credits">Credits : Muhammad Ridwan Na'im & Anrie 'Riesurya' Suryaningrat</p>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
